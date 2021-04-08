2b69f06f723cf468ecfb5ccbb6491806462d1757
MisaelB7 <misaelbanegas07@gmail.com>
2021-04-07 21:45:46 -0600

    rediseño para correcto funcionamiento en movil

--- src/screens/forgotPassword.js
+++ src/screens/forgotPassword.js
@@ -1,102 +1,186 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions} from 'react-native';
-import Logo from '../shared/Logo'
+import createDataContext from "./createDataContext";
 import { firebase } from "../firebase";
-const {width, height} = Dimensions.get("screen");
-import theme from '../theme/index'
-import { validate } from "email-validator";
-import Alert from '../shared/Alert'
-import { Input,Button } from "react-native-elements";
-import PopUpMessage from '../shared/PopUpMessage';
 
-const forgotPassword = ({ navigation }) => {
-    const [email, setEmail] = useState("");
-    const [emailError, setEmailError] = useState(false);
-    const [error, setError] = useState("");
-    const [visible, setVisible] = useState(false);
-    const logoTitle ="Success"
-    const successMessage = "A password recovery message was sent to the email you provided."
-    const hintMessage = "If you are unable to find it please do check your Junk/Spam folder."
+// Acciones disponibles para el reducer
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "errorMessage":
+      return { ...state, errorMessage: action.payload };
+    case "signin":
+      return { ...state, user: action.payload, loggedIn: true };
+    case "signout":
+      return { ...state, user: action.payload, loggedIn: false };
+    case "persistLogin":
+      return {
+        ...state,
+        user: action.payload.user,
+        loggedIn: action.payload.loggedIn,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
 
-    const handleVerify = (input) => {
-        if (input === "email") {
-          if (!email) setEmailError(true);
-          else if (!validate(email)) setEmailError(true);
-          else setEmailError(false);
-        }
-    };
-    const handleRecovery = () =>{
-        firebase
-            .auth()
-            .sendPasswordResetEmail(email)
-            .then(()=>{
-                navigation.navigate("Signin");
-                })
-            .catch((error) => {
-                setError(error.message)
-            })
+// Permite el inicio de sesión mediante firebase con email y password
+const signin = (dispatch) => (email, password) => {
+  // Hacer la petición al API de firebasee
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((response) => {
+      // Obtener el Unique Identifier generado para cada usuario
+      // Firebase -> Authentication
+      const uid = response.user.uid;
+
+      // Obtener la colección desde Firebase
+      const usersRef = firebase.firestore().collection("users");
+
+      // Verificar que el usuario existe en Firebase authentication
+      // y también está almacenado en la colección de usuarios.
+      usersRef
+        .doc(uid)
+        .get()
+        .then((firestoreDocument) => {
+          if (!firestoreDocument.exists) {
+            dispatch({
+              type: "errorMessage",
+              payload: "User does not exist in the database!",
+            });
+          } else {
+            // Llamar el reducer y enviarle los valores del usuario al estado
+            dispatch({ type: "errorMessage", payload: "" });
+            dispatch({ type: "signin", payload: firestoreDocument.data() });
+          }
+        });
+    })
+    .catch((error) => {
+      dispatch({ type: "errorMessage", payload: error.message });
+    });
+};
+
+// Cierra la sesión del usuario
+const signout = (dispatch) => () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      dispatch({ type: "signout", payload: {} });
+    })
+    .catch((error) => {
+      dispatch({ type: "errorMessage", payload: error.message });
+    });
+};
+
+// Verifica si existe el token de firebase para iniciar sesión sin credenciales
+const persistLogin = (dispatch) => () => {
+  const userRef = firebase.firestore().collection("users");
+
+  // Si el usuario ya se ha autenticado previamente, retornar
+  // la información del usuario, caso contrario,retonar un objeto vacío.
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      userRef
+        .doc(user.uid)
+        .get()
+        .then((document) => {
+          dispatch({
+            type: "persistLogin",
+            payload: { user: document.data(), loggedIn: true },
+          });
+        })
+        .catch((error) => {
+          dispatch({ type: "errorMessage", payload: error.message });
+        });
+    } else {
+      dispatch({
+        type: "persistLogin",
+        payload: { user: {}, loggedIn: false },
+      });
     }
+  });
+};
 
-    return (
-        <View style={styles.container}>
-            <Logo/>
-            {error ? <Alert title={error} type="error" /> : null}
-            <Input
-                inputContainerStyle={{borderBottomWidth:0}} inputStyle={styles.input}
-                placeholder="   Email"
-                autoCapitalize="none"
-                value={email}
-                onChangeText={setEmail}
-                onBlur={() => {
-                    handleVerify("email");
-                }}
-                errorMessage={
-                    emailError
-                    ? "Please verify your accounts email address"
-                    : null
-                }
-            />
-            <Button buttonStyle={styles.button} title="Verify" onPress={handleRecovery} />
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Signin")}
-            >
-                <Text style={styles.goBack}>Go back</Text>
-            </TouchableOpacity>
-            {/* Success Message of the password recovery email */}
-            <PopUpMessage 
-                navigation={navigation} 
-                navigationScreen="Signin" 
-                visibleState={visible} 
-                logoTitle={logoTitle} 
-                successMessage={successMessage} 
-                hintMessage={hintMessage} />
-        </View>
-    )
+const isUserEqual = (googleUser, firebaseUser) => {
+  if (firebaseUser) {
+    const providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-const styles = StyleSheet.create({
-   button: {
-        width: 110,
-        backgroundColor: theme.colors.secondary,
-        margin: 15,
-        borderRadius: 50,
-        alignSelf: "Center",
-      },
-      goBack: {
-        textAlign: "center",
-      },
-      input:{
-        marginTop:50,
-        borderRadius: 50,
-        borderStyle: "Solid",
-        border:1,
-      },
-      container: {
-        flex: 1,
-        justifyContent: "center",
-        padding: 10,
-        backgroundColor: theme.colors.backgroundWhite,
-      },
-});
+const signInWithGoogle = (dispatch) => (googleUser) =>{
+  const unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+    unsubscribe();
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      const credential = firebase.auth.GoogleAuthProvider.credential(
+        googleUser.idToken,
+        googleUser.accessToken
+      );
+      firebase
+        .auth()
+        .signInWithCredential(credential)
+        .then((response) => {
+          const uid = response.user.uid;
+          const email = response.user.email
+          const fullname = response.user.displayName
+          const data = {
+            id: uid,
+            email,
+            fullname,
+          };
+          const usersRef = firebase.firestore().collection("users");
+          usersRef
+          .doc(uid)
+          .get()
+          .then((firestoreDocument) => {
+            if (!firestoreDocument.exists) {
+              usersRef
+                .doc(uid)
+                .set(data)
+                .then(() => {
+                  dispatch({
+                    type: "signup",
+                    payload: { user: data, registered: true },
+                  });
+                })
+                .catch((error) => {
+                  dispatch({ type: "errorMessage", payload: error.message });
+                });
+            } else {
+              dispatch({ type: "errorMessage", payload: "" });
+              dispatch({ type: "signin", payload: firestoreDocument.data() });
+            }
+          });
+        })
+        .catch((error) => { 
+          dispatch({ type: "errorMessage", payload: error.message });
+        });
+    } else {
+      console.log("User already signed-in Firebase.");
+    }
+  });
+}
 
-export default forgotPassword;
+
+// Exportar las funcionalidades requeridas al contexto
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  {
+    signin,
+    signout,
+    persistLogin,
+    signInWithGoogle,
+  },
+  {
+    user: {},
+    errorMessage: "",
+    loggedIn: false,
+    loading: true,
+  }
+);
