fc20563acf6c5186a273550334f63088427555eb
Melva-99 <melvarobertina.83@outlook.com>
2021-04-13 22:59:00 -0600

    agregando el theme para dark mode

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,44 +1,63 @@
 import React, { useContext, useEffect } from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Context as AuthContext } from "../../providers/AuthContext";
 import Signin from "../../screens/Signin";
 import Signup from "../../screens/Signup";
 import Home from "../../screens/Home";
 import forgotPassword from "../../screens/forgotPassword";
+import Donate from "../../screens/Donate";
+import Donate_Online from "../../screens/Donate_Online";
+import putUpForAdoption from "../../screens/putUpForAdoption";
+import Adopt from "../../screens/Adopt";
+import { useColorScheme } from "react-native";
 
 const Stack = createStackNavigator();
 
 const Navigation = () => {
   const { state, persistLogin } = useContext(AuthContext);
-
-  // Verificar si ya existen credenciales de autenticación
   useEffect(() => {
     persistLogin();
   }, []);
 
+  const scheme = useColorScheme();
+  const MyDarkTheme = {
+    dark:true,
+    colors:{
+      primary:"#ffffff",
+      background: "#242d42",
+      card:"#242d42",
+      text: "#ffffff",
+      border:"#242d42",
+    }
+  }
   return (
-    <NavigationContainer>
-      {!state.loading && (
-        <>
-          {state.loggedIn ? (
-            <Stack.Navigator>
-              <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-          ) : (
-            <Stack.Navigator>
-              <Stack.Screen
-                name="Signin"
-                component={Signin}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen name="Signup" component={Signup} />
-              <Stack.Screen name="forgotPassword" component={forgotPassword}  options={{headerShown: false,headerStyle:{backgroundColor:"#085A75"} }} />
-            </Stack.Navigator>
-          )}
-        </>
-      )}
-    </NavigationContainer>
+    <NavigationContainer
+    theme={scheme ==="dark" ? MyDarkTheme:DefaultTheme}>
+    {!state.loading && (
+      <>
+        {state.loggedIn ? (
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Donate" component={Donate} />
+            <Stack.Screen name="Donate_Online" component={Donate_Online}/>
+            <Stack.Screen name="putUpForAdoption" component={putUpForAdoption}/>
+            <Stack.Screen name="Adopt" component={Adopt}/>
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Signin"
+              component={Signin}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Signup" component={Signup} />
+            <Stack.Screen name="forgotPassword" component={forgotPassword}  options={{headerShown: false,headerStyle:{backgroundColor:"#085A75"} }} />
+          </Stack.Navigator>
+        )}
+      </>
+    )}
+  </NavigationContainer>
   );
 };
 
