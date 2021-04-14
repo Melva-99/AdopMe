5bf94248524fa0f97b75be87443e5f8f11aa74bc
Melva-99 <melvarobertina.83@outlook.com>
2021-04-13 22:55:46 -0600

    agregar el onPress de los botones

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, } from "react";
 import { Button, Text } from "react-native-elements";
-import { StyleSheet, View, } from "react-native";
+import { StyleSheet, View } from "react-native";
 import theme from "../theme";
 import { Context as AuthContext } from "../providers/AuthContext";
 
 const Home = ({ navigation }) => {
   const { signout } = useContext(AuthContext);
+
   return (
     <>
     <View style={styles.container}>
       <Text style={styles.titulo}>AdopMe</Text>
-      <Button buttonStyle={styles.button} title="Adopt"/>
-      <Button buttonStyle={styles.button} title="Put up for adoption"/>
-      <Button buttonStyle={styles.button} title="Donate"/>
+      <Button buttonStyle={styles.button} title="Adopt" onPress={() => {navigation.navigate("Adopt")}}/>
+      <Button buttonStyle={styles.button} title="Put up for adoption" onPress={() => {navigation.navigate("putUpForAdoption")}}/>
+      <Button buttonStyle={styles.button} title="Donate" onPress={() => {navigation.navigate("Donate")}}/>
       <Button buttonStyle={styles.button} title="Sign Out" onPress={() => {signout()}}/>
     </View>
     </>
@@ -25,7 +26,7 @@ const styles = StyleSheet.create({
     marginTop:120,
   },
   button:{
-    width: 150,
+    width: 250,
     backgroundColor: theme.colors.secondary,
     marginBottom:80,
     borderRadius: 50,
@@ -35,6 +36,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom:50,
     fontSize:30,
+    marginTop:-30,
+    color:theme.colors.secondary,
   },
 });
 
